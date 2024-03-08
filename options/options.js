763796ea3c6a8dec2ea64642e0b44ded86684e38
Crowdbotics <team@crowdbotics.com>
2024-03-08 22:59:50 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "steep-heart-46901",
+  url: "https://steep-heart-46901.botics.co",
+  api: "https://steep-heart-46901.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
